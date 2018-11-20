223f4ebeb4c0704460568b2c09e48f35f1e7c79e
zhangyongwang <349368992@qq.com>
2018-11-20 09:46:25 +0800

    路由、中间件、静态目录的设定

--- app.js
+++ app.js
@@ -19,16 +19,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/student', function (req, res, next) {
+  res.send('hello zhangyongwang')
+})
+app.use('/stylesheets/style.html', function (req, res, next) {
+  console.log('get style.css.........')
+  res.send('get style.css.........')
+})
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with a resource 7777');
 });
 
+router.get('/abc', function(req, res, next) {
+  res.send('respond with a resource abcabcabc');
+});
+
+router.get('/def', function(req, res, next) {
+  res.send('respond with a resource defdefdef');
+}); 
 module.exports = router;

b78c8cb7c9afddd20eb7ae3ee56eaf76edcf1922
zhangyongwang <349368992@qq.com>
2018-11-22 11:30:34 +0800

    前端业务模块

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -1,3 +1,15 @@
-var Toast = require('../mod/toast.js').Toast
+require('less/index.less');
 
-Toast('hello world!')
+var NoteManager = require('mod/note-manager.js').NoteManager;
+var Event = require('mod/event.js');
+var WaterFall = require('mod/waterfall.js');
+
+NoteManager.load();
+
+$('.add-note').on('click', function() {
+  NoteManager.add();
+})
+
+Event.on('waterfall', function(){
+  WaterFall.init($('#content'));
+})

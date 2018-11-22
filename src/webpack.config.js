2314cc951a2689f7835f9b908b4d8fc6e76cd202
zhangyongwang <349368992@qq.com>
2018-11-22 08:23:12 +0800

    Toast 组件

---
+++ src/webpack.config.js
@@ -0,0 +1,47 @@
+var webpack = require('webpack')
+var path = require('path')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var autoprefixer = require('autoprefixer')
+
+module.exports = {
+    entry: path.join(__dirname, "js/app/index.js"),
+    output: {
+        path: path.join(__dirname, "../public"),
+        filename: "javascript/index.js"
+    },
+    module: {
+        rules: [{
+            test: /\.less$/,
+            use: ["style-loader", "css-loader", "less-loader", "postcss-loader"]
+
+        }]
+    },
+    resolve: {
+        alias: {
+            jquery: path.join(__dirname, "js/lib/jquery.min.js"),
+            mod: path.join(__dirname, "js/mod"),
+            less: path.join(__dirname, "less")
+        }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery"
+        }),
+        new ExtractTextPlugin("css/index.css"),
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                postcss: [
+                    autoprefixer(),
+                ]
+            }
+        })
+        // new webpack.optimize.UglifyJsPlugin({
+        //     compress: {
+        //         warnings: false,
+        //     },
+        //     output: {
+        //         comments: false,
+        //     },
+        // }),
+    ]
+};
